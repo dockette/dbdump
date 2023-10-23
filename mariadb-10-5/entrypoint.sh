6accc5909fcdbb4bfe7b74c3d0d3c45e272018a3
ondrejj <ondrej.janota@moderntv.eu>
2023-10-23 17:40:40 +0200

    Docker: change path to docker-entrypoint for mariadb 10.5 image
    - mariadb docker image 10.4 has symlinks to docker-entrypoint.sh
    + call docker-entrypoint.sh directly from its location

--- mariadb-10-5/entrypoint.sh
+++ mariadb-10-5/entrypoint.sh
@@ -2,7 +2,7 @@
 set -eo pipefail
 set -x
 
-/docker-entrypoint.sh mysqld 1>/srv/output.log 2>&1 &
+/usr/local/bin/docker-entrypoint.sh mysqld 1>/srv/output.log 2>&1 &
 
 wait-for-it localhost:3306 --timeout=30 --strict 1>/srv/output.log 2>&1
 
