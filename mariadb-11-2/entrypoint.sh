e79562f72a6f17fbdee30a670693eb47d5caaea7
Milan Felix Šulc <rkfelix@gmail.com>
2024-07-19 17:38:54 +0200

    Add mariadb 10.6, 10.11, 11.1, 11.2, 11.4, 11.5

---
+++ mariadb-11-2/entrypoint.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+set -eo pipefail
+set -x
+
+/docker-entrypoint.sh mysqld 1>/srv/output.log 2>&1 &
+
+wait-for-it localhost:3306 --timeout=30 --strict 1>/srv/output.log 2>&1
+
+exec "$@"
